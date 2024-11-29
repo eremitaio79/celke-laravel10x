a28c5715b7f846ffda2dcfa35c0c5fc63f891b0e
Paulo Eremita <eremitaio79@gmail.com>
2024-11-29 13:41:15 -0300

    CourseController and layout was created

--- resources/views/layout/template1.blade.php
+++ resources/views/layout/template1.blade.php
@@ -4,25 +4,92 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel10x Celke Project</title>
+
+    <title>@yield('title')</title>
+
     <link href="{{ asset('css/bootstrap533/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fontawesome671/all.css') }}" rel="stylesheet">
 </head>
 
 <body>
 
+    <div class="container-fluid bg-secondary sticky-top">
+        {{-- CONTAINER START --}}
+        <div class="container">
+            {{-- NAVBAR START --}}
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="{{ url('/') }}" target="_self">CELKE</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="{{ url('/') }}"
+                                    target="_self">Home</a>
+                            </li>
+                            {{-- <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li> --}}
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Módulos
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="{{ route('course.index') }}" target="_self"><i
+                                                class="fa-solid fa-book"></i>&nbsp;&nbsp;Cursos...</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#"><i
+                                                class="fa-solid fa-user"></i>&nbsp;&nbsp;Usuários...</a></li>
+                                </ul>
+                            </li>
+                            {{-- <li class="nav-item">
+                                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li> --}}
+                        </ul>
+                        {{-- <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form> --}}
+                    </div>
+                </div>
+            </nav>
+            {{-- NAVBAR END --}}
+        </div>
+        {{-- CONTAINER END --}}
+    </div>
+
     <div class="container">
-        <div class="row">
+        <div class="row mt-4 mb-4">
             <div class="col-12">
+
+                {{-- CARD START --}}
+                <div class="card text-center">
+                    <div class="card-header">
+                        @yield('title_card')
+                    </div>
+                    <div class="card-body">
+                        @yield('content')
+                    </div>
+                    <div class="card-footer text-body-secondary">
+                        @yield('footer_card')
+                    </div>
+                </div>
+                {{-- CARD END --}}
 
             </div>
         </div>
     </div>
 
 
-
-    <script src="{{ asset('js/bootstrap533/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{ asset('js/fontawesome671/all.js')}}"></script>
+    <script src="{{ asset('js/bootstrap533/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/fontawesome671/all.js') }}"></script>
 </body>
 
 </html>
