4b5464763b904c7279e6f04f1a3c8c283f0cdc58
Paulo Eremita <eremitaio79@gmail.com>
2024-11-29 10:41:02 -0300

    The first migration course was created

---
+++ database/migrations/2024_11_29_121637_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            // PE: My fields.
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
+            $table->boolean('status')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
