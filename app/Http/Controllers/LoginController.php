2a1994afe156af0ff13f6ea8b9541e7e7794594b
Paulo Eremita <eremitaio79@gmail.com>
2024-12-17 15:46:06 -0300

    I implemented the LoginController, login view and adjusted template1 to include the logout link. I also included the logged in user information.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -14,9 +16,42 @@ class LoginController extends Controller
         return view('');
     }
 
+    /**
+     * Process login.
+     */
+    public function loginProcess(Request $request, LoginRequest $loginRequest)
+    {
+        $loginRequest->validated();
+        // dd($request);
+
+        $authenticated = Auth::attempt([
+            'email' => $loginRequest->email,
+            'password' => $loginRequest->password
+        ]);
+
+        if (!$authenticated) {
+            return back()->withInput()->with('msgError', 'Ocorreu um erro ao autenticar seus dados de acesso. Tente novamente.');
+        }
+
+        return redirect()->route('home')->with('msgSuccess', 'Autenticado com sucesso!');
+    }
+
+    /**
+     * Open homepage after login.
+     */
     public function home()
     {
         return view('homecelke');
+    }
+
+    /**
+     * System logout.
+     */
+    public function logoutProcess()
+    {
+        Auth::logout();
+
+        return redirect()->route('root')->with('msgSuccess', 'Seu acesso foi encerrado com sucesso!');
     }
 
     /**
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,14 +2,18 @@
 
 
 @section('title', 'ACESSO')
-@section('title_card', 'Acesso ao Sistema')
+
+@section('title_card')
+    <i class="fa-solid fa-user"></i>&nbsp;Acesso ao Sistema
+@endsection
 
 @section('footer_card')
     <div class="row">
         <div class="col-12">
             Caso ainda não possua usuário, <strong>
-                <a href="#" target="_self" class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
-                     clique aqui
+                <a href="#" target="_self"
+                    class="link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">
+                    clique aqui
                 </a>
             </strong> para cadastrar.
         </div>
@@ -29,6 +33,28 @@
 
 
 @section('content')
+
+    {{-- Request validations --}}
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            @foreach ($errors->all() as $error)
+                {!! $error !!}<br />
+            @endforeach
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
+    <script>
+        // Aguarda 5 segundos e remove o alerta
+        setTimeout(() => {
+            const alert = document.querySelector('.alert');
+            if (alert) {
+                alert.classList.remove('show'); // Esconde o alerta
+                alert.classList.add('fade'); // Aplica a transição de saída
+                setTimeout(() => alert.remove(), 300); // Remove do DOM após a animação
+            }
+        }, 5000); // 5 segundos
+    </script>
 
     {{-- MSG ERROR --}}
     @if (session('msgError'))
@@ -88,21 +114,31 @@
     </script>
 
     <div class="row justify-content-center align-items-center">
-        <div class="col-4">
-            <form action="" id="formLogin" method="POST" enctype="multipart/form-data" target="_self">
-                <div class="row mb-3">
+        <div class="col-3">
+            <form action="{{ route('login.process') }}" id="formLogin" method="POST" enctype="multipart/form-data"
+                target="_self">
+                @csrf
+                @method('POST')
+
+                <div class="row mb-2">
                     <div class="col-12 text-start">
-                        <label for="username" class="for">E-mail de Acesso</label>
-                        <input type="text" id="username" name="username" class="form-control"
-                            placeholder="Informe o e-mail incluído no cadastro" required />
+                        <label for="email" class="for">E-mail de Acesso</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="basic-addon1">
+                                <i class="fa-solid fa-envelope"></i>
+                            </span>
+                            <input type="email" id="email" name="email" class="form-control" aria-label="email"
+                                aria-describedby="basic-addon1" value="{{ old('email') }}"
+                                placeholder="Informe o e-mail incluído no cadastro">
+                        </div>
                     </div>
                 </div>
 
-                <div class="row mb-3">
+                <div class="row mb-4">
                     <div class="col-12 text-start">
                         <label for="password" class="for">Senha de Acesso</label>
                         <input type="password" id="password" name="password" class="form-control"
-                            placeholder="Informe a senha de acesso" required />
+                            value="{{ old('password') }}" placeholder="Informe a senha">
                     </div>
                 </div>
 
