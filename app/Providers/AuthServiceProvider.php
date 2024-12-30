d1d62157de29d46462c02a9d86d0c5f48a6197fa
Paulo Eremita <eremitaio79@gmail.com>
2024-12-30 10:14:35 -0300

    The feature to send an email for password reset has been implemented.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,8 +24,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->registerPolicies();
+        // $this->registerPolicies();
 
-        //
+        ResetPassword::createUrlUsing(function (User $user, string $token) {
+            return env('APP_URL') . "/password-reset/$token";
+        });
     }
 }
